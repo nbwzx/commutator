7b459ec69ff6bee2c15ad44e233ab3fb39fb5c6d
nbwzx <55343466+nbwzx@users.noreply.github.com>
2022-04-30 15:55:31 +0800

    Update cookie.js

--- assets/dist/js/cookie.js
+++ assets/dist/js/cookie.js
@@ -1,31 +1,33 @@
+"use strict";
+
 function setCookie(cname, cvalue, exdays) {
-    var d = new Date();
-    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-    var expires = "expires=" + d.toGMTString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";";
+    const d = new Date();
+    d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
+    const expires = `expires=${d.toGMTString()}`;
+    document.cookie = `${cname}=${cvalue};${expires};`;
 }
 
 function getCookie(cname) {
-    var name = cname + "=";
-    var decodedCookie = decodeURIComponent(document.cookie);
-    var ca = decodedCookie.split(';');
-    for (var i = 0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') {
+    const name = `${cname}=`;
+    const decodedCookie = decodeURIComponent(document.cookie);
+    const ca = decodedCookie.split(";");
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) === " ") {
             c = c.substring(1);
         }
-        if (c.indexOf(name) == 0) {
+        if (c.indexOf(name) === 0) {
             return c.substring(name.length, c.length);
         }
     }
     return "";
 }
 
-window.onload = function loadCookie() {
+window.onload = function onload() {
     if (getCookie("settingsOuterBracket") === "true") {
         byid("settingsOuterBracket").checked = true;
     }
-}
+};
 
 function changeCookie() {
     if (byid("settingsOuterBracket").checked === true) {
